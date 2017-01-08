8fb18a1b1ff1e96e1e63a1c323067d8700de9b01
Alexey Sudachen <alexey@sudachen.name>
2017-01-08 17:13:16 -0300

    simplified uccm.cmd

--- util/getuccm.Src/makeit.cmd
+++ util/getuccm.Src/makeit.cmd
@@ -1,9 +1,10 @@
 @echo off
 cmd /c javac uccm/GetUccm.java
-cmd /c jar -cvfe ../../getuccm100.jar uccm.GetUccm uccm/GetUccm.class
+cmd /c jar -cvfe getuccm100.jar uccm.GetUccm uccm/GetUccm.class
+copy /B uccm.cmd + getuccm100.jar ..\..\uccm.cmd
 cd ../..
 del /q uccm100-dist.zip 
-cmd /c jar -cMf uccm100-dist.zip getuccm100.jar uccm.cmd
+cmd /c jar -cMf uccm100-dist.zip uccm.cmd
 
 
 
